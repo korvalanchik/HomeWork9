0ffbc9dd086192e00f02ab82bdcb64e395f785e9
korvalanchik <korvalan@yahoo.com>
2023-09-11 16:36:20 +0300

    HashMap release 1.0 ArrayList release 1.1 LinkedList release 1.0

---
+++ MyHashMap.java
@@ -0,0 +1,103 @@
+package HW9;
+
+import javax.management.openmbean.InvalidKeyException;
+import javax.management.openmbean.KeyAlreadyExistsException;
+import java.util.Arrays;
+
+public class MyHashMap<K, V> {
+    private static final int MAP_CAPACITY = 8;
+    private Entry<K, V>[] entries = new Entry[MAP_CAPACITY];
+    private int size;
+
+    public void put(K key, V value) {
+        int index = bucketIndex(key);
+        Entry<K, V> entry = new Entry<>(key, value, null);
+        if(entries[index] == null) {
+            entries[index] = entry;
+        } else {
+            if(entries[index].getKey().equals(key)) {
+                throw new KeyAlreadyExistsException();
+            }
+            while (entries[index].next != null) {
+                if(entries[index].next.getKey().equals(key)) {
+                    throw new KeyAlreadyExistsException();
+                }
+                entries[index] = entries[index].next;
+            }
+            entries[index].next = entry;
+        }
+        size++;
+    }
+
+    public V get(K key) {
+        int index = bucketIndex(key);
+        if(entries[index] != null) {
+            if (entries[index].getKey().equals(key)) {
+                return entries[index].value;
+            } else {
+                while (entries[index].next != null) {
+                    entries[index] = entries[index].next;
+                    if (entries[index].getKey().equals(key)) {
+                        return entries[index].value;
+                    }
+                }
+            }
+        }
+        return null;
+//        throw new InvalidKeyException();
+    }
+
+    public void remove(K key) {
+        int index = bucketIndex(key);
+        if(entries[index].getKey().equals(key)) {
+            if(entries[index].next != null) {
+                entries[index] = entries[index].next;
+            } else {
+                entries[index] = null;
+            }
+            size--;
+            return;
+        } else {
+            while (entries[index].next != null) {
+                if(entries[index].next.getKey().equals(key)) {
+                    entries[index] = entries[index].next.next;
+                    size--;
+                    return;
+                } else {
+                    entries[index] = entries[index].next;
+                }
+            }
+        }
+        throw new InvalidKeyException();
+    }
+
+    public void clear(){
+        Arrays.fill(entries, null);
+        size = 0;
+    }
+
+    private int bucketIndex(K key) {
+        return Math.abs(key.hashCode()) % entries.length;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static class Entry<K, V> {
+        private K key;
+        private V value;
+        private Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+    }
+}
