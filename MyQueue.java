a7622eda9d517b3895f9ac968649c8e4eca3d0e8
korvalanchik <korvalan@yahoo.com>
2023-09-11 20:02:04 +0300

    HashMap release 2.0

---
+++ MyQueue.java
@@ -0,0 +1,42 @@
+package HW9;
+
+import java.util.Objects;
+
+public class MyQueue <T> extends MyLinkedList<T>{
+    private MyQueue.Node<T> head;
+    private MyQueue.Node<T> tail;
+    private int size;
+    public void add(T value) {
+        MyQueue.Node<T> node = new MyQueue.Node<>(value);
+        if(head == null){
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    public T peek() {
+        if(head == null) return null;
+        return head.value;
+
+    }
+
+    public T poll() {
+        if(head == null) return null;
+        T tempHead = head.value;
+        head = head.next;
+        head.previous = null;
+        size--;
+        return tempHead;
+    }
+
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+
+}
